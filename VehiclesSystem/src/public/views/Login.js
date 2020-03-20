97a6dfa0b255a802133e9f91d73ca42e376141f4
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-03-20 16:12:30 -0400

    Adicionado fundo azul para a tela de Login

--- VehiclesSystem/src/public/views/Login.js
+++ VehiclesSystem/src/public/views/Login.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default class Login extends Component{
     render() {
         return(
-            <View>
+            <View style={styles.container}>
                 <Text>Página de Login</Text>
             </View>
         );
     }
 }
+
+const styles =  StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#3498db'
+    } 
+});
