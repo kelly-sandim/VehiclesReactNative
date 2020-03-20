cbed826d9e56d7e4dd93f9605b2303b66b2cf28d
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-03-20 15:59:16 -0400

    RouterConfig de volta, mas como um arquivo em branco pois não consegui corrigir/configurar ele, então está sendo utilizado o PublicRouter temporariamente

--- VehiclesSystem/src/app/index.js
+++ VehiclesSystem/src/app/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
 import PublicRouter from '../public/routes/PublicRouter';
 
-const App = () => <PublicRouter />;
 
-export default App;
+const AppStackNavigator = PublicRouter;
+
+const Apps = createAppContainer(AppStackNavigator)
+
+export default class App extends React.Component {
+    render() {
+        return <Apps />;
+    }
+}
+
+// const App = () => <PublicRouter />;
+
+// export default App;
