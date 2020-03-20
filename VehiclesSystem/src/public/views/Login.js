30e8a3a07eb1b381bc60a64060c8ec54f9b401fe
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-03-20 16:31:00 -0400

    Adicionado logo e TextInputs para a tela de login

--- VehiclesSystem/src/public/views/Login.js
+++ VehiclesSystem/src/public/views/Login.js
@@ -6,8 +6,19 @@ export default class Login extends Component{
     render() {
         return(
             <View style={styles.container}>
-                <View style={styles.logoContainer}></View>
-                <View style={styles.formContainer}></View>
+                <View style={styles.logoContainer}>
+                    <Image style={styles.logo} source={require('../../../assets/orangeCar.png')}/>
+                </View>
+                <View style={styles.formContainer}>
+                    <TextInput 
+                        placeholder="E-mail"
+                        placeholderTextColor="rgba(255,255,255,0.7)"
+                        style={styles.input} />
+                    <TextInput 
+                        placeholder="Senha"
+                        placeholderTextColor="rgba(255,255,255,0.7)"
+                        style={styles.input} />
+                </View>
             </View>
         );
     }
@@ -17,5 +28,24 @@ const styles =  StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#3498db'
-    } 
+    },
+    logoContainer: {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },    
+    logo: {
+        width: 300,
+        height: 121
+    },
+    formContainer: {
+        padding: 20
+    },
+    input: {
+        height: 40,
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        marginBottom: 20,
+        color: '#fff',
+        paddingHorizontal: 10
+    }
 });
