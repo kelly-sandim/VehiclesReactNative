0b75954c26d87c2b378522d60483d2d987c751cb
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-03-21 19:01:28 -0400

    Adicionado rota Register no arquivo PublicRouter.js

--- VehiclesSystem/src/public/routes/PublicRouter.js
+++ VehiclesSystem/src/public/routes/PublicRouter.js
@@ -1,9 +1,13 @@
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Login from '../views/Login';
+import Register from '../views/Register';
 
 export default createStackNavigator({
     Home: {
         screen: Login
-    },        
+    },
+    Register: {
+        screen: Register
+    }        
 });
