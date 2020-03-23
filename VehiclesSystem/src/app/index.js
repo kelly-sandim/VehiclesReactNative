7e5ca8c7847b5e9f507f99603e3827454747a4fb
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-03-23 09:08:17 -0400

    Removendo comentários e imports desnecessários do index.js

--- VehiclesSystem/src/app/index.js
+++ VehiclesSystem/src/app/index.js
@@ -1,32 +1,6 @@
-import React from 'react';
-
 import PublicRouter from '../public/routes/PublicRouter';
 import { createAppContainer } from 'react-navigation';
-
-import { View, StatusBar, StyleSheet } from 'react-native';
-
 
 const App = createAppContainer(PublicRouter);
 
 export default App;
-
-// export default class App extends React.Component {
-//     render() {
-//         return( 
-//             <View style={styles.container}>
-//                 <StatusBar 
-//                     backgroundColor="#2e87c2"
-//                     barStyle="light-content"
-//                 />
-//                 <PublicRouter />    
-//             </View>
-//         );
-//     }
-// }
-
-// const styles =  StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#3498db'
-//     },
-// });
